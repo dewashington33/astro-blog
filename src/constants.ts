6b6d7c3c0de75ad236756aabf6401671dd417292
dewashington33 <dewashington@hotmail.com>
2024-03-18 16:10:02 -0400

    Add navbar titles to MainLayout and Navbar components

--- src/constants.ts
+++ src/constants.ts
@@ -3,3 +3,5 @@ export const SITE_DESCRIPTION =
   "Articles, stories and tutorials from TechPeople";
 export const HOMEPAGE_ARTICLE_LIMIT = 7;
 export const ARTICLES_PER_PAGE = 6;
+export const NAVBAR_TITLE_1 = "Perry Area";
+export const NAVBAR_TITLE_2 = "Historical Society";
