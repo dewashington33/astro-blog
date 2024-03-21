612bcd713af36b3bec727505a3c961f1b14e7b2f
dewashington33 <dewashington@hotmail.com>
2024-03-20 23:36:17 -0400

    Add output configuration to astro.config.mjs and update search form action***
    ***Add utility function capitalize to utils.ts***
    
    ***Update ArticleCard.astro to use Tags component***
    
    ***Update articles/index.astro to include SearchForm and Pagination components***
    
    ***Update Footer.astro to dynamically generate category links

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,6 @@
+//  this file contains utility functions that can be used in multiple places in the app
+
+// Format a date to be human-readable
 function formatDate(date: Date) {
   return new Date(date).toLocaleDateString("en-US", {
     year: "numeric",
@@ -6,4 +9,10 @@ function formatDate(date: Date) {
   });
 }
 
-export { formatDate };
+// Capitalize the first letter of a string
+function capitalize(str: string) {
+  if (typeof str !== "string" || str.length === 0) return str;
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+export { formatDate, capitalize };
